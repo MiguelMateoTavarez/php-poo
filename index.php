0cfac734f94f401fd222468637310ee58a50eddf
Miguel Mateo <miguelmateot94@gmail.com>
2021-06-28 18:59:42 -0400

    Interaction between objects

--- index.php
+++ index.php
@@ -1,36 +1,112 @@
 <?php
 
-abstract class Unit {
-    protected $alive = true;
+abstract class Unit
+{
+    protected $hp;
     protected $name;
 
-    public function __construct($name)
+    public function __construct($name, $hp = 40)
     {
+        $this->hp = $hp;
         $this->name = $name;
     }
-    
+
+    public function show($message)
+    {
+        echo "<p>$message</p>";
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getHp()
+    {
+        return $this->hp;
+    }
+
+    private function setHp($hp)
+    {
+        $this->hp = $hp;
+        $this->show(
+            "{$this->name} ahora tiene {$this->hp} puntos de vida"
+        );
+    }
+
     public function move($direction)
     {
-        echo "<p>{$this->name} camina hacia $direction</p>";
+        $this->show(
+            "<p>{$this->name} camina hacia $direction</p>"
+        );
     }
 
-    abstract public function attack($opponent);
-}
+    abstract public function attack(Unit $opponent);
 
-class Soldier extends Unit {
-    public function attack($opponent)
+    public function takeDamage($damage)
     {
-        echo "<p>{$this->name} corta a $opponent en dos</p>";
+
+        $this->setHp($this->hp - $damage);
+
+        if ($this->hp <= 0) {
+            $this->die();
+        }
+    }
+
+    public function die()
+    {
+        $this->show(
+            "<p>{$this->name} muere</p>"
+        );
     }
 }
 
-class Archer extends Unit{
-    public function attack($opponent)
+class Soldier extends Unit
+{
+    protected $damage = 40;
+
+    public function attack(Unit $opponent)
     {
-        echo "<p>{$this->name} dispara una flecha a $opponent</p>";
+        $this->show(
+            "<p>{$this->name} corta a {$opponent->getName()} en dos</p>"
+        );
+
+        $opponent->takeDamage($this->damage);
+    }
+
+    public function takeDamage($damage)
+    {
+        return parent::takeDamage($damage / 2);
+    }
+}
+
+class Archer extends Unit
+{
+
+    protected $damage = 20;
+
+    public function attack(Unit $opponent)
+    {
+        $this->show(
+            "<p>{$this->name} dispara una flecha a {$opponent->getName()}</p>"
+        );
+
+        $opponent->takeDamage($this->damage);
+
+    }
+
+    public function takeDamage($damage)
+    {
+        if(rand(0, 1) == 1) {
+            return parent::takeDamage($damage);
+        }
     }
 }
 
 $sander = new Archer('Sander');
+$azura = new Soldier('Azura');
+
 $sander->move('el norte');
-$sander->attack('Azura');
+$sander->attack($azura);
+$sander->attack($azura);
+$azura->attack($sander);
